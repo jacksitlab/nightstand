071f087048e54ee1349ade26157b491aa6232ef4
Jack <jack@jacks-it-lab.de#>
2020-04-24 09:18:35 +0200

    add config changed handler

--- config/config.py
+++ config/config.py
@@ -1,18 +1,52 @@
 import json
+from datetime import datetime, timedelta
 from .key import KeyConfig
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
-class NightStandConfig:
+class NightStandConfig(FileSystemEventHandler):
 
     def __init__(self, filename):
         self.filename = filename
+        self.last_modified = datetime.now()
+        self.changeListeners = []
+        eventhandler = self
+        self.fileObserver = Observer()
+        self.fileObserver.schedule(
+            self, path='./', recursive=False)
+        self.fileObserver.start()
 
     def load(self):
         with open(self.filename) as json_data:
             self.data = json.load(json_data)
 
-    def registerFilechangedLister(self, listener):
-        pass
+    def on_modified(self, event):
+        if datetime.now() - self.last_modified < timedelta(seconds=1):
+            return
+        else:
+            self.last_modified = datetime.now()
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+        if str(event.src_path).endswith(self.filename):
+            self.onConfigChanged()
+
+    def onConfigChanged(self):
+        print("config has changed")
+        self.load()
+        for listener in self.changeListeners:
+            listener()
+
+    def registerFilechangedListener(self, listener):
+        self.changeListeners.append(listener)
+
+    def unregisterFileChangedListener(self, listener):
+        self.changeListeners.remove(listener)
+
+    def join(self):
+        self.fileObserver.join()
+
+    def stop(self):
+        self.fileObserver.stop()
 
     def getKeyConfig(self, index):
         keys = self.data["keys"]
--- nightstand.py
+++ nightstand.py
@@ -24,7 +24,7 @@ class Nightstand:
 
     def init(self):
         self.config.load()
-        self.config.registerFilechangedLister(self.onConfigChanged)
+        self.config.registerFilechangedListener(self.onConfigChanged)
         # create the i2c object for the trellis
         i2c_bus = busio.I2C(SCL, SDA)
         # create the trellis
@@ -37,25 +37,27 @@ class Nightstand:
             # set all keys to trigger the blink callback
             self.trellis.callbacks[i] = self.onKeyPressed
 
-    def reset(self):
-        for i in range(16):
-            self.trellis.pixels[i] = RED
-            time.sleep(0.1)
-        for i in range(16):
-            self.trellis.pixels[i] = YELLOW
-            time.sleep(0.1)
-        for i in range(16):
-            self.trellis.pixels[i] = GREEN
-            time.sleep(0.1)
-        for i in range(16):
-            self.trellis.pixels[i] = OFF
-            time.sleep(0.1)
+    def reset(self, startupSequence = True):
+        if startupSequence:
+            for i in range(16):
+                self.trellis.pixels[i] = RED
+                time.sleep(0.1)
+            for i in range(16):
+                self.trellis.pixels[i] = YELLOW
+                time.sleep(0.1)
+            for i in range(16):
+                self.trellis.pixels[i] = GREEN
+                time.sleep(0.1)
+            for i in range(16):
+                self.trellis.pixels[i] = OFF
+                time.sleep(0.1)
         for i in range(16):
             self.trellis.pixels[i] = self.config.getKeyConfig(i).color
             time.sleep(0.1)
 
     def onConfigChanged(self):
         print("config changed")
+        self.reset(False)
 
     def onKeyPressed(self, event):
         # turn the LED on when a rising edge is detected
@@ -78,11 +80,15 @@ class Nightstand:
     def startCLI(self):
         print('Hello from the Nightstand Server')
         self.reset()
-        while True:
-            # call the sync function call any triggered callbacks
-            self.trellis.sync()
-            # the trellis can only be read every 17 millisecons or so
-            time.sleep(0.02)
+        try:
+            while True:
+                # call the sync function call any triggered callbacks
+                self.trellis.sync()
+                # the trellis can only be read every 17 millisecons or so
+                time.sleep(0.02)
+        except KeyboardInterrupt:
+            self.config.stop()
+        self.config.join()
 
 
 if __name__ == '__main__':
